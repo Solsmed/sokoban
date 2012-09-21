a0e7372823e234cda142dd87dc4cc9a5f6c8a564
Alexander Solsmed <solsmed@kth.se>
2012-09-21 22:51:44 +0200

    getPossibleEndBoards()

--- src/MoveValidator.java
+++ src/MoveValidator.java
@@ -16,4 +16,29 @@ public class MoveValidator {
 		
 		return validPermutations;
 	}
+	
+	public static Set<GameBoard> getPossibleEndBoards(GameBoard board) {
+		Set<GameBoard> possibleEndBoards = new HashSet<GameBoard>();
+		
+		GameBoard endBoard = board.getEndBoard();
+		
+		int dx[] = new int[] {0,1,0,-1};
+		int dy[] = new int[] {-1,0,1,0};
+		
+		for(int b = 0; b < endBoard.boxes.size(); b++) {
+			Point boxPoint = endBoard.boxes.get(b);
+			int bx = boxPoint.getX();
+			int by = boxPoint.getY();
+			
+			for(int d = 0; d < dx.length; d++) {
+				if(board.isWalkable(bx+dx[d],by+dy[d])) {
+					GameBoard newBoard = new GameBoard(board);
+					newBoard.player = new Point(bx+dx[d],by+dy[d]);
+					possibleEndBoards.add(newBoard);
+				}
+			}	
+		}
+		
+		return possibleEndBoards;
+	}
 }
