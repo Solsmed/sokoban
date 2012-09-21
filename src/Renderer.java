269f30b665a5ddb2c071a078030c6f07123c461f
Alexander Solsmed <solsmed@kth.se>
2012-09-21 15:02:25 +0200

    Encapsulation

--- src/Renderer.java
+++ src/Renderer.java
@@ -3,9 +3,11 @@ public class Renderer {
 	public static void draw(GameBoard b) {
 		StringBuffer sb = new StringBuffer();
 		
-		for(int y=0;y<b.board.ySize;y++){
-			for(int x=0;x<b.board.ySize;x++){
-				if(b.board.floor[x][y])
+		Point boardSize = b.board.getSize();
+		
+		for(int y=0;y<boardSize.getY();y++){
+			for(int x=0;x<boardSize.getX();x++){
+				if(b.board.isFloor(x, y))
 					sb.append('#');
 				else
 					sb.append(' ');
--- src/StaticBoard.java
+++ src/StaticBoard.java
@@ -1,12 +1,18 @@
 public class StaticBoard {
-	boolean[][] floor;
-	int xSize;
-	int ySize;
+	private boolean[][] floor;
+	private boolean[][] goal;
+	private int xSize;
+	private int ySize;
 	
-	public StaticBoard(boolean floor[][], int xSize, int ySize){
+	public StaticBoard(boolean floor[][], boolean goal[][], int xSize, int ySize){
 		this.xSize = xSize;
 		this.ySize = ySize;
 		this.floor = floor;
+		this.goal = goal;
+	}
+	
+	public boolean isFloor(int x, int y) {
+		return isFloor(new Point(x, y));
 	}
 	
 	public boolean isFloor(Point p) {
@@ -16,5 +22,18 @@ public class StaticBoard {
 			System.err.println("isFloor(): no such position. " + p + ", board size: " + xSize + "," + ySize);
 			return false;
 		}
+	}
+	
+	public boolean isGoal(Point p) {
+		try {
+			return goal[p.getX()][p.getY()];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("isGoal(): no such position. " + p + ", board size: " + xSize + "," + ySize);
+			return false;
+		}
+	}
+	
+	public Point getSize() {
+		return new Point(xSize, ySize);
 	}
 }
